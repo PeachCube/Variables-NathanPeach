0177b3353517d2c9a63157adacd5b11d87416dd0
PeachCube <nathanpeach500@live.com>
2023-09-13 14:42:14 -0300

    added a health system

--- Variables-NathanPeach/Program.cs
+++ Variables-NathanPeach/Program.cs
@@ -10,7 +10,7 @@ namespace Variables_NathanPeach
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("-Variables-");
+            Console.WriteLine("-Introduction to Variables-");
             Console.WriteLine();
             
             //int = integer, integers are always whole numbers
@@ -22,7 +22,13 @@ namespace Variables_NathanPeach
             //Note, adding the = 0 at the end is a seperate step, and used as a shortcut
             int enemyValue = 250;
 
-
+            int lives = 2;
+            //maximum lives would be 5 for a fresh player
+            int health = 75;
+            //maximum hp would be 100
+            float survivalRate = (5 - lives) / (100 - health);
+            
+            
             string realName;
             string GamerTag;
             //The shortcut "string realName = "Nathan Peach"" is not used for this example
@@ -37,7 +43,6 @@ namespace Variables_NathanPeach
             score = score + enemyValue;
             //The = sign sets the variable on the left equal to the complete varibale on the right ex:
             //score = 0 + 2, therefore score = 2
-
 
             Console.WriteLine("Welcome, "+GamerTag+ " ("+realName+")");
             Console.WriteLine("Current Score: "+score);
